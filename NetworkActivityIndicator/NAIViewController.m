f4c25bfa834aaf8b52a58aece6a36de078687550
Matt Zanchelli <matt@mdznr.com>
2014-01-11 03:25:10 -0800

    Changing text and textColor on change of download state

--- NetworkActivityIndicator/NAIViewController.m
+++ NetworkActivityIndicator/NAIViewController.m
@@ -22,6 +22,7 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+// For the sake of this demo, all cells are the same.
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
 	UITableViewCell *cell = [super tableView:tableView cellForRowAtIndexPath:indexPath];
@@ -34,13 +35,19 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
 	UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+	// Not excellent, but done for simplicity of the demo project
 	UIActivityIndicatorView *spinner = (UIActivityIndicatorView *) cell.accessoryView;
 	
+	// If the individual download is active or not
 	if ( [spinner isAnimating ] ) {
 		[spinner stopAnimating];
+		cell.textLabel.text = @"Start Simulated Activity";
+		cell.textLabel.textColor = [UIColor colorWithRed:0.0f green:122.0f/255.0f blue:1.0f alpha:1.0f];
 		[[UIApplication sharedApplication] endedNetworkActivity];
 	} else {
 		[spinner startAnimating];
+		cell.textLabel.text = @"Stop Simulated Activity";
+		cell.textLabel.textColor = [UIColor colorWithRed:1.0f green:0.22f blue:0.22f alpha:1.0f];
 		[[UIApplication sharedApplication] beganNetworkActivity];
 	}
 }
