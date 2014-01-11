8b3ac626f22a0a05be1e72d3831d49670258e646
Matt Zanchelli <matt@mdznr.com>
2014-01-11 03:42:25 -0800

    Making notes of how I think the networkActivityIndicatorVisible property should work.

--- NetworkActivityIndicator/UIApplication+NetworkActivityIndicator.h
+++ NetworkActivityIndicator/UIApplication+NetworkActivityIndicator.h
@@ -14,6 +14,12 @@
  This category will automatically keep track of concurrent network activity and display the network activity indicator accordingly.
  */
 
+// This method should not exist
+- (void)setNetworkActivityIndicatorVisible:(BOOL)networkActivityIndicatorVisible NS_DEPRECATED_IOS(2_0, 7_0);
+
+// This should be readonly
+@property (nonatomic, readonly) BOOL networkActivityIndicatorVisible;
+
 #warning Documentation for when to use the Network Activity Indicator
 /// Tell the application that network activity has begun. The network activity indicator will then be shown.
 - (void)beganNetworkActivity;
